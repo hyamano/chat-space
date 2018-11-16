ee1565d3045ddc30489db2c6e07d0b534e6ca034
yamano hiroki <josemarihiroki@gmail.com>
2018-11-16 10:37:51 +0900

    delete comment
    (1) comment outの削除
    (2) console.logの削除

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -2,7 +2,6 @@ $(function() {
 
   var search_list = $("#user-search-result")
   function appendUser(user) {
-    // console.log("appendUser");
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${ user.name }</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="
@@ -12,7 +11,6 @@ $(function() {
   }
 
   function appendNoUser(Nousertext) {
-    // console.log("NoappendUser");
     var html = `<div class="chat-group-user clearfix">
   //                 <p class="chat-group-user__name">${ Nousertext }</p>`
     search_list.append(html);
@@ -20,7 +18,6 @@ $(function() {
 
   $("#user-search-field").on("keyup", function() {
     var input = $(this).val();
-    // console.log(input);
     $.ajax({
       type: 'GET',
       url: '/users',
@@ -29,20 +26,18 @@ $(function() {
     })
 
     .done(function(users){
-      // console.log('ajax通信：成功');
-      // console.log("返却値を確認：", users);
       $("#user-search-result").empty();
-      if (users.length !== 0) {
+      if(users.length !== 0){
         users.forEach(function(user){
-           appendUser(user);
+          appendUser(user);
         })
-      } else {
+      }else{
         appendNoUser("該当ユーザなし");
       }
     })
     .fail(function(){
-       alert('通信失敗');
-     })
-   });
- });
+      alert('通信失敗');
+    })
+  });
+});
 
