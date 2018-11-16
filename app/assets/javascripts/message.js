72551d5fb78836081c24c93347f6f37a9a67d772
yamano hiroki <josemarihiroki@gmail.com>
2018-11-17 01:46:52 +0900

    event_construction
    new_messageを設定
    イベントを設置

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -48,6 +48,14 @@ $(function(){
     .fail(function(){
       alert('通信失敗');
       $('.form__submit').prop('disabled',false);
-    })
+
+      var interval = setInterval(function(){
+      var current_url = window.location.href;
+      console.log(current_url);
+      var new_message = $('.message').last().attr('message-id');
+      console.log(new_message);
+    }
+  }
+
+    });
   });
-});
