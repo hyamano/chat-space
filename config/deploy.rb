4b54928bfe5513f28caece99be494fed9b716e29
h_yamano <josemarihiroki@icloud.com>
2018-11-22 18:01:09 +0900

    re:editing deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
-
 set :application, "chat-space"
 set :repo_url, "git@github.com:hyamano/chat-space.git"
-
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
@@ -26,28 +24,20 @@ set :repo_url, "git@github.com:hyamano/chat-space.git"
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
-
  # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
  # Default value for local_user is ENV['USER']
  # set :local_user, -> { `git config user.name`.chomp }
-
  # Default value for keep_releases is 5
 set :keep_releases, 5
-
  # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
-
 set :ssh_options, auth_methods: ['publickey'],
-keys: ['/Users/h_yamanoh/.ssh/chat-space.pem']
-
+            keys: ['/Users/h_yamanoh/.ssh/chat-space.pem']
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
-
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
